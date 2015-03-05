180d02c95ca8be584c3f1df50c21e73c3ae74cf9
Eric Jameson <e-jameson@bethel.edu>
2015-03-05 14:07:16 -0600

    wrong class

--- js/bu_animate.js
+++ js/bu_animate.js
@@ -186,7 +186,7 @@ var bu_fx = {
 
 
             // fade in the text aboce
-            $('js-animate').each(function (i) {
+            $('homepage-animate').each(function (i) {
                 //$(this).find('.uppercase-underlined');
                 var elem = $(this);
 
