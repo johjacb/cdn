8f80d0bbac618c899e05133927e6774b1b274510
Eric Jameson <e-jameson@bethel.edu>
2015-03-05 16:05:58 -0600

    keep js-fade-in until CSS is updated

--- js/bu_animate.js
+++ js/bu_animate.js
@@ -175,6 +175,7 @@ var bu_fx = {
 
             setTimeout(function () {
                 elem.addClass('js-animate');
+                elem.addClass('js-fade-in');
             }, 600);
 
             this.proofPointAnimationReady = false;
