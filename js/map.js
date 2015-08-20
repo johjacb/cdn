8599569d8674c7470635eb3de667e303495dfdb1
c-schwarze <ces55739@bethel.edu>
2015-08-20 15:13:17 -0500

    map -- try to use initialize method

--- js/map.js
+++ js/map.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
         $("#parking_select").children().val(parking);
 
     // reinitialize it
-    initializeSelectedMap($(this));
+    initializeSelectedMap($select);
 });
 
 function initializeSelectedMap($mapSelect) {
